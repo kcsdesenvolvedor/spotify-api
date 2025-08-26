d4d813daf98c45a571e4780f8433cc93d75bee88
kcsdesenvolvedor <kcs.desenvolvedor@gmail.com>
2025-08-25 22:49:14 -0300

    Criando interface com endpoint para buscar artistas

---
+++ SpotifyApi/SpotifyApi/Services/ISpotifyApi.cs
@@ -0,0 +1,11 @@
+﻿using Refit;
+
+namespace SpotifyApi.Services
+{
+    [Headers("accept: application/json", "Authorization: Bearer {accessToken}")]
+    public interface ISpotifyApi
+    {
+        [Get("/v1/artists/{id}")]
+        Task<string> GetArtistAsync(string id, string accessToken);
+    }
+}
