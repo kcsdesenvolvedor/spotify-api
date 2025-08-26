c136828da162d8997f8fe64e32133e80f5a7b5ef
kcsdesenvolvedor <kcs.desenvolvedor@gmail.com>
2025-08-26 10:11:15 -0300

    Criando controlador para buscar token de acesso a api

---
+++ SpotifyApi/SpotifyApi/Controllers/AuthController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Json;
+
+namespace SpotifyApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public AuthController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [HttpGet()]
+        public async Task<IActionResult> AccessToken(string clientId, string clientSecret)
+        {
+            var client = _httpClientFactory.CreateClient();
+
+            var requestBody = new Dictionary<string, string>
+        {
+            {"grant_type", "client_credentials"},
+            {"client_id", clientId},
+            {"client_secret", clientSecret}
+        };
+
+            var requestMessage = new HttpRequestMessage(HttpMethod.Post, "https://accounts.spotify.com/api/token");
+            requestMessage.Headers.Authorization = new AuthenticationHeaderValue(
+                "Basic",
+                Convert.ToBase64String(Encoding.UTF8.GetBytes($"{clientId}:{clientSecret}"))
+            );
+            requestMessage.Content = new FormUrlEncodedContent(requestBody);
+
+            var response = await client.SendAsync(requestMessage);
+            response.EnsureSuccessStatusCode();
+
+            var responseContent = await response.Content.ReadAsStringAsync();
+            var tokenInfo = JsonSerializer.Deserialize<JsonElement>(responseContent);
+            var accessToken = tokenInfo.GetProperty("access_token").GetString();
+
+            return Ok(new { AccessToken = accessToken });
+        }
+    }
+}
